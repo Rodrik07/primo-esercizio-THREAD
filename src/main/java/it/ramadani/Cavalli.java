ec04466937ba1ac996bd734fe9b0e6ab3f24dfb2
lai2-24 <lai2-24@itismeucci.com>
2025-09-29 09:38:50 +0200

    esercizio FINITO

--- src/main/java/it/ramadani/Cavalli.java
+++ src/main/java/it/ramadani/Cavalli.java
@@ -1,17 +1,17 @@
 package it.ramadani;
 
 public class Cavalli extends Thread{
-    private int cavalloNumero;
+    private int numCav;
 
-    public Cavalli(int cavalloNumero) {
-        this.cavalloNumero = cavalloNumero;
+    public Cavalli(int c) {
+        this.numCav = c;
     }
 
     public void run(){
         try {
             int tempoCorsa = (int) (Math.random() * 1000);
             Thread.sleep(tempoCorsa);
-            System.out.println(Thread.currentThread().getName() + " - Cavallo numero: " + cavalloNumero + " ha finito in " + tempoCorsa + " ms");
+            System.out.println(Thread.currentThread().getName() + " - Cavallo numero: " + numCav + " ha finito in " + tempoCorsa + " ms");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
